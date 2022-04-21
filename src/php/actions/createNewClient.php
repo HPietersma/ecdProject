68b37dbeb92cbdd1c3a1d8a011d1c3e61bdc1393
HPietersma <hessel.pietersma@hotmail.nl>
2022-04-21 19:42:33 +0200

    bezig met queries

--- src/php/actions/createNewClient.php
+++ src/php/actions/createNewClient.php
@@ -3,6 +3,8 @@
 $request_body = file_get_contents('php://input');
 $data = json_decode($request_body, true);
 
+$client_id;
+
 $voornaam = "";
 $achternaam = "";
 $telefoon = "";
@@ -10,6 +12,10 @@ $geboortedatum = "";
 $email = "";
 $adres = "";
 $plaats = "";
+
+$reanimeren = true;
+
+$aandoeningen = array();
 
 // NAW
     if (isset($data['NAW']['voornaam'])) {
@@ -34,17 +40,84 @@ $plaats = "";
         $plaats = $data['NAW']['plaats'];
     };
 
+// REANIMEREN
+    if (isset($data['reanimeren'])) {
+        $reanimeren = $data['reanimeren'];
+    };
 
-    $sql = "INSERT INTO clienten (voornaam, achternaam, geboortedatum, telefoon, email, adres, plaats)
-            VALUES ('$voornaam', '$achternaam', '$geboortedatum', '$telefoon', '$email', '$adres', '$plaats')
+// AANDOENINGEN
+    if (isset($data['aandoeningen'])) {
+        //$aandoeningen = $data['aandoeningen']['item'];
+        foreach ($data['aandoeningen'] as $key => $value) {
+            $aandoeningen[] = $value;
+        }
+    };
+
+
+
+
+// QUERIES
+    $sql_clienten = 
+        "INSERT INTO clienten (voornaam, achternaam, geboortedatum, telefoon, email, adres, plaats)
+        VALUES ('$voornaam', '$achternaam', '$geboortedatum', '$telefoon', '$email', '$adres', '$plaats')
+    ";
+
+
+    if (isset($data)) {
+        if (mysqli_query($con, $sql_clienten) == true) {
+            $client_id = mysqli_insert_id($con);
+        }
+        else {
+            die();
+        }
+    }
+    else {
+        die();
+    };
+
+    // REANIMEREN
+        $sql_reanimeren = 
+            "INSERT INTO reanimeren (client_id, reanimeren)
+            VALUES ('$client_id', '$reanimeren')
+        ";
+        mysqli_query($con, $sql_reanimeren);
+
+    // AANDOENINGEN
+        foreach ($aandoeningen as $key => $value) {
+            $item = $value['item'];
+            $sql_aandoeningen = 
+                "INSERT INTO aandoeningen (client_id, aandoening)
+                VALUES ('$client_id', '$item')
             ";
+            mysqli_query($con, $sql_aandoeningen);
+        }
 
-    $res = mysqli_query($con, $sql);   
 
 
-    $json = array(
-        "succes"=>false,
-        "data"=>$data,
-    );
+
+
+ 
+$json = array(
+"succes"=>true,
+"data"=>$data,
+"test"=>$aandoeningen
+);
+    
+
+
+
+   
+   
+   
+   
+    //$res = mysqli_query($con, $sql_reanimeren);   
+    
+
+
+
+    // $json = array(
+    //     "succes"=>false,
+    //     "data"=>$data,
+    // );
 
 ?>
